0b40d27b15fdb9b0293eb37df2327d973d47fe18
Randy Ruiz <randyruiz@Randys-MBP.fios-router.home>
2022-05-17 14:06:48 -0400

    final

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/datetime', (req, res) => {
+  res.send(new Date())
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/myname', (req, res) => {
+  res.send('Randy Ruiz');
+});
+
+router.get('/myfavoritemovies', (req, res) => {
+  const arr = ['Grandmas Boy, How High, Jay and Silent Bob Strike Back']
+
+  res.send(arr);
+});
+
 module.exports = router;
